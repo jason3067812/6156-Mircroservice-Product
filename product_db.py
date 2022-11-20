013bc9ee86f6ed6d4ce1b49f5b1c11a83825344d
Hao-Hsin Hsu <haohsin0303@gmail.com>
2022-11-20 16:40:43 -0500

    UPD: Update global variable, add staticmethod

--- product_db.py
+++ product_db.py
@@ -1,28 +1,34 @@
 import pymysql
 
 
+USER = "admin"
+PASSWORD = "dbuserdbuser"
+HOST = "db1.cyl4xx6wpuab.us-east-1.rds.amazonaws.com"
+TABLE_NAME = "f22_databases.product"
+
+
 class Products:
 
     def __int__(self):
-        self.table_name = "f22_databases.product"
-        self.user = "admin"
-        self.password = "dbuserdbuser"
-        self.host = "db1.cyl4xx6wpuab.us-east-1.rds.amazonaws.com"
+        pass
 
 
-    def _get_connection(self):
+    def _get_connection():
 
         conn = pymysql.connect(
-            user=self.user,
-            password=self.password,
-            host=self.host,
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
             cursorclass=pymysql.cursors.DictCursor,
             autocommit=True
         )
         return conn
 
 
-    def _execute(self, sql, args):
+    def get_products():
+
+        sql = "SELECT * FROM " + TABLE_NAME;
+        args = ()
         conn = Products._get_connection()
         cur = conn.cursor()
         res = cur.execute(sql, args=args)
@@ -30,46 +36,45 @@ class Products:
         return result
 
 
-    def get_products(self):
-
-        sql = "SELECT * FROM " + self.table_name;
-        args = ()
-        result = _execute(sql)
-
-        return result
-
-
     def get_product_by_id(self, product_id):
 
-        sql = "SELECT * FROM " + self.table_name + " WHERE id=%s";
+        sql = "SELECT * FROM " + TABLE_NAME + " WHERE id=%s";
         args = (product_id)
-        result = _execute(sql, args)
-
+        conn = Products._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=args)
+        result = cur.fetchone()
         return result
 
 
     def add_product(self, name, description, price, inventory, image):
 
-        sql = "INSERT INTO " + self.table_name + " VALUES (%s, %s, %f, %s, %s)";
+        sql = "INSERT INTO " + TABLE_NAME + " VALUES (%s, %s, %f, %s, %s)";
         args = (name, description, price, inventory, image)
-        result = _execute(sql, args)
-
+        conn = Products._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=args)
+        result = cur.fetchone()
         return result
 
 
     def update_product(self, product_id, name, description, price, inventory, image):
         
-        sql = "UPDATE " + self.table_name + " SET name=%s, description=%s, price=%s, inventory=%s, image=%s WHERE id=%d";
+        sql = "UPDATE " + TABLE_NAME + " SET name=%s, description=%s, price=%s, inventory=%s, image=%s WHERE id=%d";
         args = (name, description, price, inventory, image, product_id)
-        result = _execute(sql, args)
-
+        conn = Products._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=args)
+        result = cur.fetchone()
         return result
 
 
     def delete_product(self, product_id):
 
-        sql = "DELETE FROM " + self.table_name + " WHERE id=%d";
+        sql = "DELETE FROM " + TABLE_NAME + " WHERE id=%d";
         args = (product_id)
-        result = _execute(sql, args)
-
+        conn = Products._get_connection()
+        cur = conn.cursor()
+        res = cur.execute(sql, args=args)
+        result = cur.fetchone()
         return result
