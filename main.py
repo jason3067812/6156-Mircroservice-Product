becff01fa78c645f97c43d37305e89c1f5bc9c4a
Hao-Hsin Hsu <haohsin0303@gmail.com>
2022-11-20 16:26:29 -0500

    UPD: delete unused code, add product db functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from flask import Flask, Response, request
-from datetime import datetime
 import json
-from columbia_student_resource import ColumbiaStudentResource
+from product_db import Products
 from flask_cors import CORS
+
 
 # Create the Flask application object.
 app = Flask(__name__,
@@ -12,67 +12,56 @@ app = Flask(__name__,
 
 CORS(app)
 
-#########################################################################################
-# sample code
 
-@app.get("/api/health")
-def get_health():
-    t = str(datetime.now())
-    msg = {
-        "name": "F22-Starter-Microservice",
-        "health": "Good",
-        "at time": t
-    }
-
-    # DFF TODO Explain status codes, content type, ... ...
-    result = Response(json.dumps(msg), status=200, content_type="application/json")
-
-    return result
-
-
-@app.route("/api/students/<uni>", methods=["GET"])
-def get_student_by_uni(uni):
-
-    result = ColumbiaStudentResource.get_by_key(uni)
-
-    if result:
-        rsp = Response(json.dumps(result), status=200, content_type="application.json")
-    else:
-        rsp = Response("NOT FOUND", status=404, content_type="text/plain")
-
-    return rsp
-######################################################################################################
-# start here
-
-@app.route("/get_product", methods=['GET'])
+@app.route("/get_products", methods=['GET'])
 def get_products():
-    print("get product pass")
-    return "get_products"
+    result = Products.get_products()
+    if result:
+        resp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        resp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return resp
 
 
 @app.route("/get_product/<id>", methods=['GET'])
 def get_product_by_id(id):
-    print(f"get product by id: {id} pass")
-    return f"get_product_by_id: {id}"
+    result = Products.get_product_by_id(id)
+    if result:
+        resp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        resp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return resp
 
 
-@app.route("/post_product", methods=['POST'])
-def add_product():
-    print("add product pass")
-    return "add_product"
+@app.route("/add_product", methods=['POST'])
+def add_product(name, description, price, inventory, image):
+    result = Products.add_product(name, description, price, inventory, image)
+    if result:
+        resp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        resp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return resp
 
 
-@app.route("/update_product", methods=['GET'])
-def update_product():
-    print("update product pass")
-    return "update_product"
+@app.route("/update_product", methods=['POST'])
+def update_product(product_id, name, description, price, inventory, image):
+    result = Products.update_product(product_id, name, description, price, inventory, image)
+    if result:
+        resp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        resp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return resp
 
 
-@app.route("/delete_product", methods=['GET'])
-def delete_product():
-    print("delete product pass")
-    return "delete_product"
+@app.route("/delete_product", methods=['POST'])
+def delete_product(id):
+    result = Products.delete_product(id)
+    if result:
+        resp = Response(json.dumps(result), status=200, content_type="application.json")
+    else:
+        resp = Response("NOT FOUND", status=404, content_type="text/plain")
+    return resp
+
 
 if __name__ == "__main__":
     app.run(port=5000, debug=True)
-    
